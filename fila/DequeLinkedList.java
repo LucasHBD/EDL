d257edf213fabea1796a12c2bea5261f4ce55fe9
LucasHBD <130990709+LucasHBD@users.noreply.github.com>
2025-10-01 20:22:39 +0000

    :construction: wip: implementando fila com lista duplamente ligada

---
+++ fila/DequeLinkedList.java
@@ -0,0 +1,28 @@
+public class DequeLinkedList{
+    private No inicio;
+    private No fim;
+
+    public class No{
+        private Object elemento;
+        private No proximo;
+        private No anterior;
+    }
+
+
+    public DequeDuplamenteLigado(){
+        this.inicio = null;
+        this.fim = null;
+    }
+
+    
+    public int inserirInicio(Object o){
+        if()
+        
+        No novo = new No();
+        novo.elemento = o;
+        novo.proximo = No.inicio;
+        novo.anterior = null;
+    }
+
+}
+
